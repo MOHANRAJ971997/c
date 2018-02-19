5e75b6c56ecf5c440163dd296c26d9e7799ae9b3
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-02-19 13:17:24 +0530

    Create mohan61.c

---
+++ mohan61.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int i, j, k, count = 0;
+    char str[50];
+ 
+    printf("enter string\n");
+    scanf("%[^\n]s", str);
+    k = strlen(str);
+    printf("prime characters in a string are\n");
+    for (i = 2;i <= k;i++)
+    {    
+        count = 0;
+        for (j = 2;j <= k;j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            printf("%c\n", str[i - 1]);
+        }
+    }
+getch()
+}
