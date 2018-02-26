322e1de76a89ee61ec636be03a804408fe9c6834
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-02-26 13:18:28 +0530

    Create mohan74.c

---
+++ mohan74.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rou{
+     public static void main(String aa[]){
+        float d;
+        Scanner s=new Scanner(System.in);
+        d=s.nextFloat();
+      int v=  Math.round(d);
+      System.out.println(v);
+  
+}
+    }
+    getch();
