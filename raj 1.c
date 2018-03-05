71f7b240d4c1024e98dccdd7d35d19e2115ca734
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-03-05 13:50:23 +0530

    Create raj 1.c

---
+++ raj 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+a=int(input("\nEnter the number:"))
+b=input("\nEnter the string:")
+c=list(b)
+d=0
+while a>0:
+    print(c[d],end="")
+    d=d+1
+    a=a-1
+    }
+     getch();
