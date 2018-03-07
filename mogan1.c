c147fb8115a280d45bbb5456b3235c47caa67a34
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-03-07 14:01:58 +0530

    Create mogan1.c

---
+++ mogan1.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+#include<conio.h>
+int fact (int);
+int main()
+{
+    int n,c,i=0;
+    scanf("%d",&n);
+    printf("Factorial of the number %d is: %d",n,fact(n));
+    return 0;
+}
+int fact(int x)
+{
+    if(x==1||x==0)
+    return 1;
+    else
+    return (x*fact(x-1));
+}
+getch();
