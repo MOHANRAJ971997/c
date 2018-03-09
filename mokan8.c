1f9755d02f92f64b1a9e51088790b6248e3fa0e8
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-03-09 15:51:45 +0530

    Create mokan8.c

---
+++ mokan8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,a[10],i,j,k;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			for(int k=j+1;k<n;k++)
+			{
+				if(a[i]+a[j]==a[k])
+				{
+					printf("%d %d %d\n",a[i],a[j],a[k]);
+				}
+			}
+		}
+	}
+
+getch();
+}
